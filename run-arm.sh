7f951973335fba043a0076c99d5977f2410fd7f0
migueltc13 <73550106+migueltc13@users.noreply.github.com>
2026-02-27 14:30:07 +0000

    Add slurm job for ARM partition

---
+++ run-arm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=chk_arm
+#SBATCH --nodes=1
+#SBATCH --exclusive
+#SBATCH --partition=dev-arm
+#SBATCH --account=f202500010hpcvlabuminhoa
+#SBATCH --time=00:10:00
+#SBATCH --output=out/chk_arm_%j.out
+
+source venv/bin/activate
+
+python3 -m train.train
