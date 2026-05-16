76f078c4fd93b86668b5c86dac3ded91fe18f227
migueltc13 <73550106+migueltc13@users.noreply.github.com>
2026-05-16 21:40:31 +0100

    feat: made download_assets.py executable and add --assets-dir argument

--- src/train/download_assets.py
+++ src/train/download_assets.py
@@ -1,11 +1,17 @@
+#!/usr/bin/env python3
+
 import os
-from transformers import AutoTokenizer, AutoModelForMaskedLM, AutoModelForCausalLM
+import argparse
+from transformers import (
+    AutoTokenizer,
+    AutoModelForMaskedLM,
+    AutoModelForCausalLM,
+)
+
 from datasets import load_dataset
 
-def download_assets():
-    # TODO: Update the base_dir to your desired path
-    assets_dir = "/projects/F202500010HPCVLABUMINHO/josesoares/pca/assets"
-    
+
+def download_assets(assets_dir: str):
     models_to_download = {
         "bert-base-uncased": (AutoModelForMaskedLM, "bert-base-uncased"),
         "Qwen/Qwen2.5-1.5B": (AutoModelForCausalLM, "qwen-1.5B")
@@ -14,22 +20,43 @@ def download_assets():
     for repo_id, (model_class, folder_name) in models_to_download.items():
         dest_path = os.path.join(assets_dir, "models", folder_name)
         os.makedirs(dest_path, exist_ok=True)
-        
+
         print(f"\n--- Download: {repo_id} ---")
-        tokenizer = AutoTokenizer.from_pretrained(repo_id, trust_remote_code=True)
-        model = model_class.from_pretrained(repo_id, trust_remote_code=True)
-        
+        tokenizer = AutoTokenizer.from_pretrained(
+            repo_id,
+            trust_remote_code=True
+        )
+
+        model = model_class.from_pretrained(
+            repo_id,
+            trust_remote_code=True
+        )
+
         tokenizer.save_pretrained(dest_path)
         model.save_pretrained(dest_path)
         print(f"Saved in: {dest_path}")
 
     dataset_dest = os.path.join(assets_dir, "datasets", "wikitext")
     os.makedirs(dataset_dest, exist_ok=True)
-    
-    print(f"\n--- Download: Wikitext ---")
+
+    print("\n--- Download: Wikitext ---")
     dataset = load_dataset("wikitext", "wikitext-2-raw-v1")
     dataset.save_to_disk(dataset_dest)
     print(f"Dataset saved in: {dataset_dest}")
 
+
 if __name__ == "__main__":
-    download_assets()
+    arg_parser = argparse.ArgumentParser(
+        description="Download models and datasets for training."
+    )
+
+    arg_parser.add_argument(
+        "--assets-dir",
+        type=str,
+        required=True,
+        help="Directory to save downloaded assets (models and datasets)."
+    )
+
+    args = arg_parser.parse_args()
+
+    download_assets(assets_dir=args.assets_dir)
